3ca6e3915bf77e4c2aee48c8fb06565789e6ed76
Athman Gude <athmangude@gmail.com>
2015-12-16 15:04:35 +0300

    added reference to example twitter code >cleaned up code

--- index.ios.js
+++ index.ios.js
@@ -7,60 +7,9 @@
 var React = require('react-native');
 var {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Component
 } = React;
 
-var Router = require('react-native-router');
+// var Router = require('react-native-router');
+var TwitterApp = require('./node_modules/react-native-router/twitter-example');
 
-class HelloPage extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <Text>Hello World</Text>
-        );
-    }
-}
-
-var firstRoute = {
-    name: 'Welcome',
-    component: HelloPage
-}
-
-class NavigationExperimentOne extends Component {
-    constructor(props) {
-        super(props)
-    }
-
-    render() {
-        return(
-            <Router firstRoute={firstRoute} />
-        );
-    }
-}
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('NavigationExperimentOne', () => NavigationExperimentOne);
+AppRegistry.registerComponent('NavigationExperimentOne', () => TwitterApp);
